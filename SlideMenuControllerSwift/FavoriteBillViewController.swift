f2c0a5e83b448367880d9ad067e78f22717a995c
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-11-06 20:06:54 -0800

    Adding menu for favorites.

---
+++ SlideMenuControllerSwift/FavoriteBillViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FavoriteBillViewControllwe.swift
+//  SlideMenuControllerSwift
+//
+//  Created by Manish Dwibedy on 11/6/16.
+//  Copyright © 2016 Yuji Hato. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteBillViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func openMenu(_ sender: UIBarButtonItem) {
+        self.slideMenuController()?.openLeft()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
---
+++ SlideMenuControllerSwift/FavoriteLegislatorViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FavoriteLegislatorViewController.swift
+//  SlideMenuControllerSwift
+//
+//  Created by Manish Dwibedy on 11/6/16.
+//  Copyright © 2016 Yuji Hato. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteLegislatorViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func openMenu(_ sender: UIBarButtonItem) {
+        self.slideMenuController()?.openLeft()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
