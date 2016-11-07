699d43b526db27dcf3d68a0dda5638b094a9dd47
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-11-07 00:05:58 -0800

    Adding Alamofire

--- SlideMenuControllerSwift/LegStateViewController.swift
+++ SlideMenuControllerSwift/LegStateViewController.swift
@@ -7,11 +7,22 @@
 //
 
 import UIKit
+import Alamofire
 
 class LegStateViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        Alamofire.request("https://httpbin.org/get").responseJSON { response in
+            print(response.request)  // original URL request
+            print(response.response) // HTTP URL response
+            print(response.data)     // server data
+            print(response.result)   // result of response serialization
+            
+            if let JSON = response.result.value {
+                print("JSON: \(JSON)")
+            }
+        }
     }
     
     @IBAction func openMenu(_ sender: UIBarButtonItem) {
