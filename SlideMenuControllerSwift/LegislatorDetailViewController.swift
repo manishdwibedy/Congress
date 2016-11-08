5cd4e55f2b371ef61049a87a30c91bd5303750f4
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-11-08 01:17:10 -0800

    Adding links.

--- SlideMenuControllerSwift/LegislatorDetailViewController.swift
+++ SlideMenuControllerSwift/LegislatorDetailViewController.swift
@@ -51,7 +51,34 @@ class LegislatorDetailViewController: UIViewController, UITableViewDelegate, UIT
         let cell = self.legislatorDetails.dequeueReusableCell(withIdentifier: "cell")! as! LegislatorDetailViewCell
         
         cell.title.text = self.titleValues[self.titleValue[indexPath.row]]
-        cell.value.text = self.legislatorDetail[self.titleValue[indexPath.row]]
+        if self.titleValue[indexPath.row] == "twitter_id"{
+            let linkAttributes = [
+                NSLinkAttributeName: NSURL(string: "http://twitter.com/" + self.legislatorDetail[self.titleValue[indexPath.row]]!)!,
+                NSForegroundColorAttributeName: UIColor.blue,
+                NSFontAttributeName : UIFont.systemFont(ofSize: 15.0)
+                ] as [String : Any]
+            
+            let attributedString = NSMutableAttributedString(string: "Twitter Link")
+            
+            attributedString.setAttributes(linkAttributes, range: NSMakeRange(0, 12))
+            cell.value.attributedText = attributedString
+        }
+        else if self.titleValue[indexPath.row] == "website"{
+            let linkAttributes = [
+                NSLinkAttributeName: NSURL(string: self.legislatorDetail[self.titleValue[indexPath.row]]!)!,
+                NSForegroundColorAttributeName: UIColor.blue,
+                NSFontAttributeName : UIFont.systemFont(ofSize: 15.0)
+                ] as [String : Any]
+            
+            let attributedString = NSMutableAttributedString(string: "Website Link")
+            
+            attributedString.setAttributes(linkAttributes, range: NSMakeRange(0, 12))
+            cell.value.attributedText = attributedString
+        }
+        else{
+            cell.value.text = self.legislatorDetail[self.titleValue[indexPath.row]]
+        }
+        
         return cell
     }
     
