7bfdb196bbb5d7531004fd2bfc1e0410676e50bc
Manish Dwibedy <manish.dwibedy@gmail.com>
2016-11-11 15:49:27 -0800

    Added the slide menu to the about page as well.

--- SlideMenuControllerSwift/AboutViewController.swift
+++ SlideMenuControllerSwift/AboutViewController.swift
@@ -12,12 +12,16 @@ class AboutViewController: UIViewController, SlideMenuControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.slideMenuController()?.addLeftGestures()
+        self.slideMenuController()?.addRightGestures()
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func openMenu(_ sender: UIBarButtonItem) {
+        self.slideMenuController()?.openLeft()
     }
 }
